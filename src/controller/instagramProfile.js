a058b96d69d482898b2fbef8024065fffd76114e
Dishang <94671110+Dishang20@users.noreply.github.com>
2022-03-09 11:04:14 +0530

    story fix

--- src/controller/instagramProfile.js
+++ src/controller/instagramProfile.js
@@ -72,7 +72,7 @@ exports.showProfile = (async (req, res) => {
             const followingUsername = new Set(following.map(({ username }) => username));
             const meNotFollowBack = followers.filter(({ username }) => !followingUsername.has(username))
             const mutualFollowers = (followers.filter(({ username }) => followingUsername.has(username)) && following.filter(({ username }) => followersUsername.has(username)))
-
+const stroryViewers = new Set(story.map(({username}) => username))
 
             /// lengths
             var mutualCount = mutualFollowers.length;
@@ -92,7 +92,7 @@ exports.showProfile = (async (req, res) => {
                 profile: loggedInUser.profile_pic_url,
                 account_type: loggedInUser.account_type,
                 postCount,
-                story,
+                story:stroryViewers,
                 posts: postData,
                 followerCount,
                 followers: followers,
