ceeb5762ea319724c3c9d9a0486f38893a3b3d82
Sachingiri Gosai <sachingosai.flexion@gmail.com>
2022-03-21 22:58:34 +0530

    authcode add

--- src/router/index.routes.js
+++ src/router/index.routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { showProfile, getAuthCode } = require('../controller/instagramProfile')
 
-router.post('/login', getAuthCode)
+router.get('/login', getAuthCode)
 router.get('/', showProfile)
 
 module.exports = router
